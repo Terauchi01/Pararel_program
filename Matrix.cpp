2a55ff9b7e5689f2e605a7631d7ae6d68d77f104
tereauchi <tereauchi@tereauchinonotobukkukonpyuta.local>
2024-12-01 07:36:57 +0900

    ヒープ領域の確保

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,17 +1,15 @@
 #include <chrono>
 #include <iostream>
 using namespace std;
-// /* execute with 4 processes */
-// #define N 4
-int N = 1000;
+int N = 2000;
 int main(int argc, char **argv) {
-  int matA[1][N];
-  int matB[N][N];
-  int matC[1][N];
+  vector<int> matA(N, 0);
+  vector<vector<int>> matB(N, vector<int>(N, 0));
+  vector<int> matC(N, 0);
   int size, rank;
   int i, j, k;
   for (i = 0; i < N; i++) {
-    matA[0][i] = 1;
+    matA[i] = 1;
   }
   for (i = 0; i < N; i++) {
     for (j = 0; j < N; j++) {
@@ -23,9 +21,9 @@ int main(int argc, char **argv) {
     for (j = 0; j < N; j++) {
       int v = 0;
       for (k = 0; k < N; k++) {
-        v += matA[i][k] * matB[k][j];
+        v += matA[k] * matB[k][j];
       }
-      matC[i][j] = v;
+      matC[j] = v;
     }
   }
   auto end = chrono::high_resolution_clock::now();
